87e6658079f7eb3d2afc5c9d1187b09bec9d073f
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-18 05:42:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,40 @@
+grocery_shelf = [
+  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+  { :item => "SALSA", :price => 1.50, :clearance => false },
+  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
+]
+
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+collection.each do |x|
+  if name == x[:item]
+    puts x
+  end
+end
+return nil
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+puts find_item_by_name_in_collection("SALSA", grocery_shelf)
 
-end
+unconsolidated_cart = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false}
+]
 
+# def consolidate_cart(cart)
+#   new_cart = []
+#   cart.each do |x|
+#     if cart [:item] > 1
+#       new_cart.push ([:count => ([:item].length)])
+#     end
+#     new_cart.push (x)
+#   end
+
+# end
+
+# puts consolidate_cart(unconsolidated_cart)
 
   
